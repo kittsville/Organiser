dcc1457f9cfaf03bf53b9bc82737e5102ffeb798
Kit Maywood <kittsville@googlemail.com>
2023-02-09 17:15:49 +0000

    Add health check

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def parseUUID(raw_uuid):
 
 urls = (
     '/lists/(.+)', 'list_management',
+    '/health', 'health',
     '/([0-9a-z\-]*)', 'homepage'
 )
 cacheBust   = int(time.time())
@@ -23,6 +24,13 @@ app         = web.application(urls, globals())
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 r = redis.from_url(redis_url, decode_responses=True)
 r.ping()
+
+class health:
+    def GET(self):
+        if r.ping():
+            return web.ok()
+        else:
+            return web.internalerror()
 
 class homepage:
     def GET(self, raw_uuid):
