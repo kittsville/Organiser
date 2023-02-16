3d5f6c8ca2483cbb763f184850c5759b73a5fa8a
Kit Maywood <kittsville@googlemail.com>
2023-02-16 19:40:13 +0000

    Make default checklist more generic
    Booo
    
    See #3

--- user.py
+++ user.py
@@ -75,7 +75,6 @@ class User:
                     'Toothbrush',
                     'Hair tie',
                     'Comb',
-                    'Progesterone',
                     'Fresh underwear'
                 ]
             }
